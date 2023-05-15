8ce01780828dfcd9c35db005c7b6d9010b3408c2
Your Name <baiyinyiteng555@gmail.com>
2023-05-16 08:16:57 +0900

    Initial commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from collections import Counter
 from urllib.parse import urlsplit
 import re
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 def remove_urls(text):
     url_regex = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
